abdfadca49c356f5d193dc421fa880ec9fc64a90
Shivaansh17 <shivansh_rastogi@hotmail.com>
2022-01-26 23:05:07 +0530

    Pagination

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,11 @@
 import "./Header.css";
 
 const Header = () => {
-  return <span className="Header">🎬 Movies World 🎥</span>
+  return (
+  <span onClick={()=>window.scroll(0,0)} className="Header">
+    🎬 Movies World 🎥
+  </span>
+  );
 };
 
 export default Header;
