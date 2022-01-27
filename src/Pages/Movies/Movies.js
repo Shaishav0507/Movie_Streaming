96e9ec22667d7322a3c6e090aef748a600f4b66f
Shivaansh17 <shivansh_rastogi@hotmail.com>
2022-01-27 22:59:08 +0530

    movie page

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Movies = () => {
+
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
+
+  const fetchMovies = async() => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+      );
+  }
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchMovies();
+    // eslint-disable-next-line
+  }, [page]);
+
   return <div>
       <span className='pageTitle'>Movies</span>
   </div>;
